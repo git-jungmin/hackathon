124acfaed8d189e1a9c7753944b2a0695d822029
jhk <qwertyjhk11@naver.com>
2025-07-28 16:19:32 +0900

    fix(cors): CORS 설정의 origin을 http://localhost:8080에서 http://localhost:8081로 변경

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:8081',
     credentials: true,
   });
 
