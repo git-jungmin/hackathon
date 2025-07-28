98f6603365c7a9070ad0fdfd8f516d7423b50850
jhk <qwertyjhk11@naver.com>
2025-07-28 12:28:48 +0900

    feat(swagger): Hackathon API에 Swagger 문서화 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { randomUUID } from 'crypto';
 if (!globalThis.crypto) {
   globalThis.crypto = { randomUUID } as any;
@@ -13,6 +14,14 @@ async function bootstrap() {
     origin: 'http://localhost:8080',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Hackathon API')
+    .setDescription('The Hackathon API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
